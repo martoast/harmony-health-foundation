46c593f3784f1b132efa496164ef16e90c06cb4c
martoast <alexmartos96@gmail.com>
2025-04-18 09:13:20 -0700

    SEO setup

--- pages/index.vue
+++ pages/index.vue
@@ -8,3 +8,16 @@
     <Contact id='contact'/>
     <Footer/>
 </template>
+
+<script setup>
+
+useSeoMeta({
+  title: 'Harmony Health Foundation',
+  ogTitle: 'Harmony Health Foundation',
+  description: 'Empowering Healthcare. Supporting Communities.',
+  ogDescription: 'Empowering Healthcare. Supporting Communities.',
+  ogImage: '/logo.jpg',
+  twitterCard: 'summary_large_image',
+})
+
+</script>
